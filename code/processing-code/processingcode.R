6c5f4fd1f42d4be47ec15ed97ffeab74e242c53c
cp-roth <cassia.roth@gmail.com>
2024-08-14 21:25:53 -0400

    Revisions for submission 2
    After call w/Bill

--- code/processing-code/processingcode.R
+++ code/processing-code/processingcode.R
@@ -51,48 +51,68 @@ skimr::skim(ML_raw)
 ## ---- cleandata --------
 # Dataset created for multiple reasons. Need to narrow down relevant variables
 # Remove unnecessary variables
-ML_1 <- ML_raw %>% dplyr::select(-VolN, -Page, -Number, -Nationality_Notes, -Maternal_Notes, -Fetal_Notes, -CauseofDeath, -PreviousHistory, -Notes)
+ML_1 <- ML_raw %>% dplyr::select(-VolN, -Page, -Number, -Nationality_Notes, -Maternal_Notes, -CauseofDeath, -PreviousHistory, -Notes)
 #Summarize missing data using dplyr
 missing_data_summary <- ML_1 %>% dplyr::summarise_all(list(~sum(is.na(.))))
 missing_data_summary
-# Extract information on gestation from Birth_Notes and create new variable gestation with singleton and twin births
+# See values for `FetalOutcome` and `Fetal_Notes` that are not missing any values for both variables
+ML_1 %>% filter(!is.na(FetalOutcome) & !is.na(Fetal_Notes))
+# Change 'FetalOutcome' to vivo if 'Fetal_Notes' indicates neonatal death (via Faleceu or faleceu in 'Fetal_Notes' column)
 ML_2 <- ML_1 %>%
-  mutate(Birth_Notes = str_trim(Birth_Notes)) %>% # removes any leading or trailing whitespace from Birth_Notes column
+  mutate(FetalOutcome = case_when(
+    FetalOutcome == "morto" & 
+      str_detect(tolower(Fetal_Notes), "faleceu") ~ "vivo",
+    TRUE ~ FetalOutcome
+  ))
+ML_2
+# Check values
+ML_2 %>% filter(!is.na(FetalOutcome) & !is.na(Fetal_Notes))
+# See how many neonatal deaths were recorded
+ML_neonatal <- ML_2 %>%
+  filter(
+    FetalOutcome == "vivo" &
+      str_detect(tolower(Fetal_Notes), "faleceu")
+  )
+# View the filtered data
+print(ML_neonatal)
+# Extract information on gestation from Birth_Notes and create new variable gestation with singleton and twin births
+ML_3 <- ML_2 %>%
+  mutate(Birth_Notes = str_trim(Birth_Notes)) %>% # removes any leading or trailing white space from Birth_Notes column
   mutate(Gestation = if_else(is.na(Birth_Notes) | Birth_Notes == "", "singleton", # checks if Birth_Notes is NA or empty strings and assigns singleton to Gestation column
                              if_else(str_detect(tolower(Birth_Notes), "gemelar"), "twin", "singleton"))) # checks for gemelar in case-insensitive manner and assigns twin accordingly or singleton otherwise
-ML_2
+ML_3
 # Remove variables with missing data not relevant to hypothesis
-ML_3 <- ML_2 %>% dplyr::select(-CivilStatus, -GestationalAge_Months, -Birth_Notes)
+ML_4 <- ML_3 %>% dplyr::select(-CivilStatus, -GestationalAge_Months, -Birth_Notes, -Fetal_Notes)
 #Summarize missing data using dplyr
-missing_data_summary <- ML_3 %>% dplyr::summarise_all(list(~sum(is.na(.))))
+missing_data_summary <- ML_4 %>% dplyr::summarise_all(list(~sum(is.na(.))))
 missing_data_summary
 # Create new variable of NBW and LBW for future logistic regression
-ML_4 <- ML_3 %>%
+ML_5 <- ML_4 %>%
   mutate(BirthweightCategory = case_when(
     Weightgrams >= 500 & FetalOutcome != "morto" & Gestation != "twin" & Weightgrams <= 2500 ~ "LBW",
     Weightgrams >= 500 & FetalOutcome != "morto" & Gestation != "twin" & Weightgrams > 2500 ~ "NBW",
     TRUE ~ NA_character_
   ))
-ML_4
+ML_5
 # Transform character variables into factors
-ML_5 <- ML_4 %>%
+ML_6 <- ML_5 %>%
   mutate(across(c(Color, Status, Nationality, Birth, MaternalOutcome, FetalOutcome, Sex, Gestation, BirthweightCategory), as.factor))
 # Check variables
-glimpse(ML_5)
+glimpse(ML_6)
 # Check levels of factor variables
-map(ML_5[c('Color', 'Status', 'Nationality', 'Birth', 'MaternalOutcome', 'FetalOutcome', 'Sex', 'Gestation', 'BirthweightCategory')], levels)
+map(ML_6[c('Color', 'Status', 'Nationality', 'Birth', 'MaternalOutcome', 'FetalOutcome', 'Sex', 'Gestation', 'BirthweightCategory')], levels)
 # Define factor variables to check frequencies
 factor_variables <- c('Color', 'Status', 'Nationality', 'Birth', 'MaternalOutcome', 'FetalOutcome', 'Sex', 'Gestation', 'BirthweightCategory')
 # Check frequencies of factor variables
-frequency_table <- ML_5 %>%
+frequency_table <- ML_6 %>%
   count(across(all_of(factor_variables)))
 print(frequency_table)
 
 ## ---- createdatasets --------
 # Create dataset for summary statistics (includes missing values)
-ML_summary <- ML_5
+ML_summary <- ML_6
 # Creating dataset for linear regression
-ML_linear <- ML_5 %>% tidyr::drop_na() %>%
+ML_linear <- ML_6 %>% tidyr::drop_na() %>%
   filter(Weightgrams >= 500, FetalOutcome != "morto", Gestation != "twin") # Drop observations of < 500 grams, all non-live births, and all twin births for regression dataset
 # Check for no missing data in linear model
 ML_linear_summary <- ML_linear %>% dplyr::summarise_all(list(~sum(is.na(.))))
